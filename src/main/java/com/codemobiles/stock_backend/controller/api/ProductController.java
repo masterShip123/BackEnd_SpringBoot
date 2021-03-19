46943ffb69528c58189963888d297055eeea9d6b
masterShip123 <acs57011210007@gmail.com>
2021-03-19 01:31:20 -0600

    33

--- src/main/java/com/codemobiles/stock_backend/controller/api/ProductController.java
+++ src/main/java/com/codemobiles/stock_backend/controller/api/ProductController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ import com.codemobiles.stock_backend.service.ProductService;
 
 @RestController
 @RequestMapping("/product")
-//@CrossOrigin
+@CrossOrigin
 public class ProductController {
 
 	private final ProductService productService;
